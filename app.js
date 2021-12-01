669648b92d552f2faabe5aa56f1cbf7214c7cef3
Parth <82073411+pg2080@users.noreply.github.com>
2021-12-01 12:14:07 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const app = express()
+const routes = require('./routes')
+const fileUpload = require('express-fileupload');
+port = 3000
+var engines = require('consolidate')
+const session = require("express-session")
+app.set('views', __dirname + '/views')
+app.engine('html', engines.mustache)
+app.set('view engine', 'html')
+app.use(express.static('public'))
+app.use(fileUpload());
+app.use(session({
+    name:'AuthCookie',
+    secret:'ParthGoti20005769',
+    resave:false,
+    saveUninitialized:true
+}))
+var Authentication = function (req, res, next) {
+    if(req.url == "/company-dashboard")
+    {
+        if(!req.session.email && !req.session.roletype == 'company')
+        {
+            res.status(403).render("unauthorized.html")
+        }
+        else{
+            next()
+        }
+        
+    }
+    else if(req.url == "/jobseeker-dashboard")
+    {
+        if(!req.session.email && !req.session.roletype == 'jobseeker')
+        {
+            res.status(403).render("unauthorized.html")
+        }
+        else{
+            next()
+        }
+        
+    }
+    else{
+        next()
+    }
+}
+var Logging = function (req, res, next) {
+    isAuthenticated = req.session.email ? "Authenticated" : "Non-Authenticated"
+    console.log("["+new Date().toUTCString()+"]: "+req.method+" "+req.originalUrl+" ("+isAuthenticated+" User)")
+    next()
+}
+app.use(Authentication)
+app.use(Logging);
+routes(app)
+app.listen(port, () => {
+    console.log("server is running...")
+    console.log("routes are listening on https://localhost:"+port)
+})

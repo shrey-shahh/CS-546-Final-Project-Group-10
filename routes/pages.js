f07b4b1912dd452aa5c2c5ea26ff5df7415694b2
yzheng14 <58087495+yzheng14@users.noreply.github.com>
2021-12-10 20:37:32 +0800

    handlebarupdate
    handlebarupdate

--- routes/pages.js
+++ routes/pages.js
@@ -1,12 +1,10 @@
 //require
 const express = require('express');
 const router = express.Router();
-
-const usersc = collections.users;
-const users = require('../data/users');
+const applications = require('../data/applications');
 const jobs = require('../data/jobs');
+const users = require('../data/users');
 const fs = require('fs');
-const { jobs } = require('../config/mongoCollections');
 router.get('/', (req, res) => {
     if(req.session.email)
     {
@@ -20,12 +18,10 @@ router.get('/', (req, res) => {
             }
     }
     else{
-        res.render('home.html');
+        res.render('partials/home', {layout : 'index'});
     }
 });
-router.get('/jobs', (req, res) => {
-        res.render('jobs.html');
-});
+
 router.get('/company-signup', (req, res) => {
     if(req.session.email)
     {
@@ -39,7 +35,7 @@ router.get('/company-signup', (req, res) => {
             }
     }
     else{
-        res.render('company-sign-up.html');
+        res.render('partials/company-sign-up', {layout : 'index'});
     }
 });
 router.get('/jobseeker-signup', (req, res) => {
@@ -55,7 +51,7 @@ router.get('/jobseeker-signup', (req, res) => {
             }
     }
     else{
-        res.render('jobseeker-sign-up.html');
+        res.render('partials/jobseeker-sign-up', {layout : 'index'});
     }
 });
 router.get('/login', (req, res) => {
@@ -71,36 +67,29 @@ router.get('/login', (req, res) => {
             }
     }
     else{
-        res.render('log-in.html');
+        res.render('partials/log-in', {layout : 'index'});
     }
 });
 router.get('/company-dashboard', (req, res) => {
     if(req.session.email && req.session.roletype == 'company')
     {
-        res.render('company-dashboard.html',{id:req.session.userid, email:req.session.email});
+        res.render('partials/company-dashboard', {layout : 'index',id:req.session.userid, email:req.session.email});
     }
     else{
-        res.render('unauthorized.html');
+        res.render('partials/unauthorized', {layout : 'index'});
     }
 });
 router.get('/jobseeker-dashboard', (req, res) => {
     if(req.session.email && req.session.roletype == 'jobseeker')
     {
-        res.render('jobseeker-dashboard.html',{id:req.session.userid, email:req.session.email});
+        res.render('partials/jobseeker-dashboard', {layout : 'index', id:req.session.userid, email:req.session.email});
     }
     else{
-        res.render('unauthorized.html');
+        res.render('partials/unauthorized', {layout : 'index'});
     }
 });
-router.get('/job-dashboard', (req, res) => {
-    if(req.session.email && req.session.roletype == 'jobseeker')
-    {
-        res.render('job-dashboard.html',{id:req.session.userid, email:req.session.email});
-    }
-    else{
-        res.render('unauthorized.html');
-    }
-});
+
+
 router.post('/signup',async (req, res) => {
     const user = req.body;
     let roletype = [];
@@ -120,6 +109,8 @@ router.post('/signup',async (req, res) => {
         throw 'Resume is not uploaded'
     }
     */
+   if(req.files)
+   {
     if(req.files.resume)
     {
     let pathresume = __dirname.split('routes')[0] + "public/resumes/"+ req.files.resume.name;
@@ -142,8 +133,9 @@ router.post('/signup',async (req, res) => {
     });
     logoname = req.files.logo.name;
     }
+}
     const check = await users.createUser(user.fullname, user.contactno, user.address, user.email, user.education, resumename, logoname, user.password, roletype);
-        if(check.applicationInserted)
+        if(check.userInserted)
         {
             res.redirect('/');
         }
@@ -152,49 +144,14 @@ router.post('/signup',async (req, res) => {
     {
         if(user.roletype == "company")
         {
-        res.status(400).render('company-sign-up.html',{error, fullname:user.fullname, contactno:user.contactno, address:user.address, education:user.education, email:user.email, password:user.password});
+        res.status(400).render('partials/company-sign-up',{layout: 'index', error, fullname:user.fullname, contactno:user.contactno, address:user.address, education:user.education, email:user.email, password:user.password});
         return false;
         }
-        res.status(400).render('jobseeker-sign-up.html',{error, fullname:user.fullname, contactno:user.contactno, address:user.address, education:user.education, email:user.email, password:user.password});
+        res.status(400).render('partials/jobseeker-sign-up',{layout: 'index', error, fullname:user.fullname, contactno:user.contactno, address:user.address, education:user.education, email:user.email, password:user.password});
         return false;
     }
     
 });
-
-router.get('/get-job/:id', async (req, res) => {
-    
-    if(req.session.email)
-    {
-      const job = await jobs.get(req.params.id);
-      const userCollection = await usersc();
-      const company = await userCollection.findOne(job.email);
-      const user = await userCollection.findOne(req.session.email);
-      res.render('job-dashboard.html',{error, jobid:job._id, companyid:company._id, seekerid: user._id, title:job.title, jobtype:job.jobtype, fieldtype:job.fieldtype, location:job.location, description:job.description, experience:job.experience, salaryrange:job.salaryrange, vacancy:job.vacancy});
-    }
-    else{
-        res.render('unauthorized.html');
-    }}
-);
-
-router.post('/apply',async (req, res) => {
-    const application = req.body;
-    let jobStatus = [];
-    jobStatus.push(jobStatus.jobStatus);
-    try{
-        const check = await applications.createApplication(application.jobId, application.jobseekerId, application.companyId, jobStatus);
-        if(check.applicationInserted){
-            res.redirect('/jobseeker-dashboard');
-        }
-    }
-    catch(error)
-    {
-        return res.status(400).json({ error });
-    }
-});
-    
-     
-
-
 router.post('/login', async (req, res) => {
     const user = req.body;
     try{
@@ -217,7 +174,7 @@ router.post('/login', async (req, res) => {
     }
     catch(error)
     {
-        res.status(400).render('log-in.html',{error, email:user.email, password:user.password});
+        res.status(400).render('partials/log-in',{layout: 'index', error, email:user.email, password:user.password});
         return;
     }
 });
@@ -225,4 +182,52 @@ router.get('/logout', (req, res) => {
     req.session.destroy();
     res.redirect('/');
 });
+
+
+
+router.get('/jobs', async (req, res) => {
+    
+    if(req.session.email)
+    {
+        const joblist = jobs.getAll();
+        res.render('partials/jobs', {layout : 'index', title:'Job Description',joblist:joblist});
+   
+   
+    }
+    else{
+        res.render('partials/unauthorized', {layout : 'index'});
+    }
+});
+
+
+router.get('/get-job/:id', (req, res) => {
+
+    if(req.session.email)
+    {
+      const job = await jobs.get(req.params.id);
+      const company = await users.getuserbyemail(job.email);
+      const user = await users.getuserbyemail(req.session.email);
+      res.render('partials/job-deciption.html',{layout : 'index', title:'Job Description',job:job, companyid:company._id, seekerid: user._id});
+    }
+    else{
+        res.render('partials/unauthorized', {layout : 'index'});
+    }
+});
+
+router.post('/apply',async (req, res) => {
+    const application = req.body;
+    let jobStatus = [];
+    jobStatus.push(application.jobStatus);
+    try{
+        const check = await applications.createApplication(application.jobId, application.jobseekerId, application.companyId, jobStatus);
+        if(check.applicationInserted){
+            res.render('partials/jobseeker-dashboard', {layout : 'index', id:req.session.userid, email:req.session.email});
+        }
+    }
+    catch(error)
+    {
+        res.render('partials/unauthorized', {layout : 'index'});
+    }
+});
+
 module.exports = router
